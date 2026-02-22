0f77d5c41410ee85c86fae907096c6b423571f2c
Andreas Marochow <Andreas.Marochow@gmail.com>
2026-02-22 23:49:17 +0100

    chore: remove server/dist from git and add to .gitignore

--- server/dist/server/src/routes/search.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../auth");
-const SearchController_1 = require("../controllers/SearchController");
-const router = (0, express_1.Router)();
-router.get('/', auth_1.authenticateToken, SearchController_1.SearchController.search);
-exports.default = router;
